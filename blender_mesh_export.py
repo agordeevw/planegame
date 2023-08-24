abc59fe5c13b197039f0cba6cc51f508cdbee5e2
Alexander Gordeev <agordeevw@gmail.com>
2023-08-25 05:22:35 +0700

    blender mesh export script

--- blender_mesh_export.py
+++ blender_mesh_export.py
@@ -1,11 +1,64 @@
 import bpy
+from ctypes import *
 
 
-def write_some_data(context, filepath, use_some_setting):
-    print("running write_some_data...")
-    f = open(filepath, 'w', encoding='utf-8')
-    f.write("Hello World %s" % use_some_setting)
+def writeMeshResource(context, filepath, use_some_setting):
+    print("running writeMeshResource...")
+    f = open(filepath, 'wb')
+    object = bpy.context.selected_objects[0]
+    depsgraph = bpy.context.evaluated_depsgraph_get()
+    object = object.evaluated_get(depsgraph)
+    mesh = object.to_mesh()
+    
+    # engine forward is -Z
+    # engine up is Y
+    # engine right is X
+    
+    # model up is Z
+    # model forward is Y
+    # model right is X
+    
+    # write vertices
+    vertices = mesh.vertices
+    vertexCount = len(vertices)
+    f.write(c_uint32(vertexCount))
+    for i in range(0, vertexCount):
+        f.write(c_float(vertices[i].co[0]))
+        f.write(c_float(vertices[i].co[2]))
+        f.write(c_float(-vertices[i].co[1]))
+        f.write(c_float(vertices[i].normal[0]))
+        f.write(c_float(vertices[i].normal[2]))
+        f.write(c_float(-vertices[i].normal[1]))
+    
+    # collect submeshes
+    submeshes = {}    
+    mesh.calc_loop_triangles()
+    for tri in mesh.loop_triangles:
+        if tri.material_index not in submeshes:
+            submeshes[tri.material_index] = []
+        submeshes[tri.material_index].append(tri.vertices[0])
+        submeshes[tri.material_index].append(tri.vertices[1])
+        submeshes[tri.material_index].append(tri.vertices[2])
+
+    # write indices
+    indexCount = 3 * len(mesh.loop_triangles)
+    f.write(c_uint32(indexCount))
+    for submeshIdx in sorted(submeshes):
+        for value in submeshes[submeshIdx]:
+            f.write(c_uint32(value))
+    
+    # write submesh descriptions
+    f.write(c_uint32(len(submeshes)))
+    submeshIndexStart = 0
+    for submeshIdx in sorted(submeshes):
+        submeshIndexCount = len(submeshes[submeshIdx])
+        f.write(c_uint32(submeshIndexStart))
+        f.write(c_uint32(len(submeshes[submeshIdx])))
+        submeshIndexStart += submeshIndexCount
+        
     f.close()
+    
+    object.to_mesh_clear()
 
     return {'FINISHED'}
 
@@ -17,16 +70,16 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
-class ExportSomeData(Operator, ExportHelper):
+class ExportMesh(Operator, ExportHelper):
     """This appears in the tooltip of the operator and in the generated docs"""
     bl_idname = "export_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Export Some Data"
+    bl_label = "Export Model"
 
     # ExportHelper mixin class uses this
-    filename_ext = ".txt"
+    filename_ext = ".meshresource"
 
     filter_glob: StringProperty(
-        default="*.txt",
+        default="*.meshresource",
         options={'HIDDEN'},
         maxlen=255,  # Max internal buffer length, longer would be clamped.
     )
@@ -50,7 +103,7 @@ class ExportSomeData(Operator, ExportHelper):
     )
 
     def execute(self, context):
-        return write_some_data(context, self.filepath, self.use_setting)
+        return writeMeshResource(context, self.filepath, self.use_setting)
 
 
 # Only needed if you want to add into a dynamic menu
@@ -60,12 +113,12 @@ def menu_func_export(self, context):
 
 # Register and add to the "file selector" menu (required to use F3 search "Text Export Operator" for quick access).
 def register():
-    bpy.utils.register_class(ExportSomeData)
+    bpy.utils.register_class(ExportMesh)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(ExportSomeData)
+    bpy.utils.unregister_class(ExportMesh)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 
